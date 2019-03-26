c914992d19193e24c69386a300089659ba6092aa
CaelumK <c_kamps@hotmail.com>
2019-03-25 21:17:41 -0400

    new features

--- app.py
+++ app.py
@@ -52,15 +52,17 @@ app.layout = html.Div(children=[
                     html.Div([
                             html.Label('Enter a name for you option', 
                                        style = {'color' : colors['text']}),
-                                       
+                            # option name           
                             dcc.Input(id = 'name', 
                                       value = 'Example', 
                                       type='text'),
-                                      
+                            # Update Available options          
                             html.Button('Add Option', id='button', n_clicks=0,
                                         style = {'color' : colors['text']}),
-                            
+                            # Select position and type
                             html.Div([
+                                
+                                # Type
                                 html.Div([dcc.RadioItems(
                                         id = 'call_put',
                                         options=[
@@ -72,7 +74,8 @@ app.layout = html.Div(children=[
                                         value = 'call',
                                         style = {'color' : colors['text']})
                                         ],className = 'six columns'),
-                            
+                                
+                                # Position
                                 html.Div([dcc.RadioItems(
                                         id = 'long_short',
                                         options=[
@@ -86,19 +89,43 @@ app.layout = html.Div(children=[
                                 ],className = 'six columns')
                             ], className='row'),
                 
+                            #%% Strike and Premium
                             html.Div([
+                                # Strike
                                 html.Div([
-                                        html.Label('Enter the option strike price', 
+                                        html.Label('Enter the option strike price:', 
                                                    style = {'color' : colors['text']}),
                                         dcc.Input(id = 'strike', 
                                                   value = 100, 
                                                   type='number')
                                         ],className = 'six columns'),
-                            
+                                
+                                # Premium
                                 html.Div([
-                                        html.Label('Enter the option premium', 
+                                        html.Label('Enter the option premium:', 
                                                    style = {'color' : colors['text']}),
                                         dcc.Input(id = 'price', 
+                                                  value = 5, 
+                                                  type='number')
+                                ],className = 'six columns')
+                            ], className='row'),
+                            
+                            #%% Underlying Price and Time to Maturity
+                            # Current price of the underlying
+                            html.Div([
+                                html.Div([
+                                        html.Label('Enter the current price of the underlying:', 
+                                                   style = {'color' : colors['text']}),
+                                        dcc.Input(id = 'underlying_price', 
+                                                  value = 100, 
+                                                  type='number')
+                                        ],className = 'six columns'),
+                            
+                                # Time to maturity 
+                                html.Div([
+                                        html.Label('Enter the time to matrurity in days:', 
+                                                   style = {'color' : colors['text']}),
+                                        dcc.Input(id = 'maturity', 
                                                   value = 5, 
                                                   type='number')
                                 ],className = 'six columns')
@@ -116,8 +143,22 @@ app.layout = html.Div(children=[
                                     value=None)
                             ], className='six columns'),
                     ], className = 'row'
-            ),   
+            ), 
             
+            html.Div(html.P('Select Figure Step Size'), 
+                     style = {'color' : colors['text']}),     
+                       
+            # Step Size Slider
+            dcc.Slider(id = 'step_size', 
+                       min = 0.25, 
+                       max = 5, 
+                       step = 0.25, 
+                       value = 2),
+            
+            # Slider output container
+            html.Div(id='slider-output-container', 
+                     style = {'color' : colors['text']}),
+           
             # Table 
             dcc.Graph(id = 'plot')
             
@@ -135,27 +176,42 @@ app.css.append_css({
                State('long_short','value'),
                State('strike','value'),
                State('price','value'),
+               State('underlying_price','value'),
+               State('maturity', 'value'),
                State('dropdown', 'options')])
 
-def update_available_options(click, name, call_put, long_short, K, P, existing_options):
+def update_available_options(click, name, call_put, long_short, K, P, underlying_price, maturity, existing_options):
     option_name = name
     existing_options.append({'label': option_name+' '
                                       +str(long_short)+' '
                                       +str(call_put)+' ' 
-                                      +str(K), 
-                             'value': option_name+' '+
-                                      str(long_short)+' '
+                                      +str(K)+' '
+                                      +str(P)+' UP = '
+                                      +str(underlying_price)+' T= '
+                                      +str(maturity), 
+                             'value': option_name+' '
+                                      +str(long_short)+' '
                                       +str(call_put)+' ' 
-                                      +str(K)+' ' 
-                                      +str(P)})
+                                      +str(K)+' '
+                                      +str(P)+' UP = '
+                                      +str(underlying_price)+' T= '
+                                      +str(maturity)})
     return existing_options
 
 @app.callback(Output('plot', 'figure'),
               [Input('dropdown', 'value')])
     
 def update_chart(active_options):
-    print(active_options)
     return
+
+
+@app.callback(
+    dash.dependencies.Output('slider-output-container', 'children'),
+    [dash.dependencies.Input('step_size', 'value')])
+
+def update_output(value):
+    return 'Step Size = "{}"'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=False)
